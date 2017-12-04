c46aa4e6d81a264b60c89c8696020fec6331e1c8
bernardoleite <up201404464@gcloud.fe.up.pt>
2017-12-04 15:50:00 +0000

    Form Validation -> Edith Profile (Still trying to solve bug on page)

--- action_editProfile.php
+++ action_editProfile.php
@@ -1,35 +1,46 @@
 <?php
 	include_once('includes/init.php');
 	include_once('database/user.php');
-	
+if (isset($_POST['submit'])) {
+
 	$username = $_SESSION['username'];
 	$name = $_POST['name'];
-	$cpassword = $_POST['cpassword'];
-	$npassword = $_POST['password'];
-	$photo = $_POST['photo'];
+	$cpassword = $_POST['cipass'];
+	$npassword = $_POST['ipass'];
+	$photo = $_POST['iphoto'];
 
-	//Caso a Password esteja errada
-	if(!isLoginCorrect($username, $cpassword) || $name=="" || $cpassword="" || npassword=="")
-	{
-		header('location:edit_profile.php'); 
+	$errorEmpty = false;
+	$errorEmail = false;
 
+	if (empty($name) || empty($cpassword) || empty($npassword) || !isLoginCorrect($username, $cpassword) ) {
+		echo "<span class='form-error'>Fill in all fields!</span>";
+		$errorEmpty = true;
 	}
-	//Altera as definições
-	else{
-
-			if(!updateUser($username, $name, $photo))
-			{
-				//Neste momento não faz nada dentro do if
-			}
-
-			if(!updatePassUser($username, $npassword))
-			{
-				//Neste momento não faz nada dentro do if
-			}
-
-			header('location:login.php');
-
+	else {
+		updateUser($username, $name, $photo);
+		updatePassUser($username, $npassword);
+		exit();
+		header('location: login.php'); 
+		echo "<span class='form-success'>All changes saved!</span>";
 	}
-	
-	exit();
+
+}
 ?>
+
+<script>
+	$("#name, #cipass, #ipass, #iphoto").removeClass( "input-error");
+
+	var errorEmpty = "<?php echo $errorEmpty; ?>";
+	var errorEmail = "<?php echo $errorEmail; ?>";
+
+	if (errorEmpty == true){
+	    $("#name, #cipass, #ipass").addClass("input-error");
+	}
+	if (errorEmpty == false){
+	    $("#name, #cipass, #ipass, #iphoto").val('');
+	}
+
+$( "button" ).click(function() {
+  $( "p" ).empty();
+});
+</script>
--- edit_profile.php
+++ edit_profile.php
@@ -1,23 +1,49 @@
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <div class="editfoto">
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+
+      $('form').submit(function (event) {
+        event.preventDefault();
+        var name = $("#name").val();
+        var cipass = $("#cipass").val();
+        var ipass = $("#ipass").val();
+        var iphoto = $("#iphoto").val();
+        var submit = $("#submit").val();
+        $(".form-message").load("action_editProfile.php", {
+          name: name,
+          cipass: cipass,
+          ipass: ipass,
+          iphoto: iphoto,
+          submit: submit
+          //window.location.href = "login.php";
+        });
+      });
+
+    });
+  </script>
 
     <form action="action_editProfile.php" method="POST">
       <p class="field">
         <label for="name"> New name: </label>
-        <input type="text" id="name" name="name"/>
+        <input type="text" id="name" name="name"/ placeholder="Type a new name">
         <br>
         <br>
       </p>
 
       <p class="field">
          <label for="ipass"> Current Password: </label>
-         <input type="password" id="cipass" name="cpassword"/>
+         <input type="password" id="cipass" name="cpassword" placeholder="Type your current Password" />
          <br>
          <br>
       </p>
 
       <p class="field">
          <label for="ipass"> New Password: </label>
-         <input type="password" id="ipass" name="password"/>
+         <input type="password" id="ipass" name="password"/ placeholder="Type your new Password">
          <br>
          <br>
       </p>
@@ -31,11 +57,11 @@
 
           <div class="editButtons">
             <ul>
-              <input type = "submit" value="Save Changes"/>
+              <input id = "submit" type = "submit" value="Save Changes"/>
               <input type = "reset" value="Cancel"/>
             </ul>
-
           </div>
+<p class="form-message"></p>
 
 
         </form>
