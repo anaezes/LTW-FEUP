6e98a0e52c8e09a8f250aec67d53f29ee76869bb
Ana Santos <anaezes@gmail.com>
2017-12-10 10:56:43 +0000

    css friends view

--- templates/user/view_friends.php
+++ templates/user/view_friends.php
@@ -1,11 +1,9 @@
 
 <div class="friends">
 	<?php foreach ($friends as $friend) { ?>
-		<div class="photo">  
-			<?php echo '<img src="img/'.$friend['usr_2'].'.png" width="100" height="100">'; ?>
-		</div>
-		<div class="nameuser">
-			<?php echo $friend['usr_2'] ?> 
+		<div class="friend">  
+			<p><?php echo '<img src="img/'.$friend['usr_2'].'.png" width="100" height="100">'; ?>
+			<p><?php echo $friend['usr_2'] ?> 
 		</div>
 		<?php } ?> 
 </div>
