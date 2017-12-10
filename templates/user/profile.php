63ecadd49d9da761f77a51b9ffeefe5655562773
miguelfAndrade <miguelandrade.96.18@gmail.com>
2017-12-10 19:01:24 +0000

    encriptacao da pass

--- templates/user/profile.php
+++ templates/user/profile.php
@@ -6,13 +6,14 @@
       </div>
 	  <br>
 	  <div class="nameuser">
-        <h2>  <?php echo $_SESSION['username']; ?> </h2>
+        <h2>username:<?php echo $_SESSION['username']; ?> </h2>
         <br>
-		<h1>  <?php echo $_SESSION['usr_name']; ?> </h1>
+		<h1>Name:  <?php echo $_SESSION['usr_name']; ?> </h1>
       </div>
 	  <div class="editButtons">
 		<ul>
 			<a href = "edit_profile.php">Edit</a>
+			<a href = "edit_profile.php">Change Password</a>
 			<a href = "index.php">Cancel</a>
 		</ul>
 	  </div>
