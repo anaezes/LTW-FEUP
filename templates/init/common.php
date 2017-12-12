1ab442b850914a08a37ebdebe5ace530821e2b9a
Ana Santos <anaezes@gmail.com>
2017-12-12 15:25:44 +0000

    add icon

--- templates/init/common.php
+++ templates/init/common.php
@@ -4,6 +4,7 @@
 		<title>ToDo List</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/style.css">
+		  <link rel="icon" type="image/png" href="/images/icon.png" sizes="16x16">
 		<link href="https://fonts.googleapis.com/css?family=Cherry+Swash" rel="stylesheet">
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
