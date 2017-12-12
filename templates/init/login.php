f7a9adeaa7e390d8b887b19cfa7d10e3c09f10c4
Ana Santos <anaezes@gmail.com>
2017-12-12 15:22:45 +0000

    change layout

--- templates/init/login.php
+++ templates/init/login.php
@@ -4,24 +4,18 @@
 					<p class="field">
 						<label for="iuser"> Username: </label>
 						<input type="text" id="iuser" name="username"/>
-
-						<br>
 						<br>
 					</p>
 					<p class="field">
 						<label for="ipass"> Password: </label>
 						<input type="password" id="ipass" name="password"/>
-						
-						<br>
 						<br>
 					</p>
-					
 					<div class="regBottons">
 						<ul>
 							<input type = "submit" value="Login"/>
 							<input type = "reset" value="Reset"/>
 						</ul>
-						<!--<a id="reg" href="registar.php">REGISTAR</a>-->
 					</div>
 			</form>
 		</div>
--- templates/init/register.php
+++ templates/init/register.php
@@ -7,40 +7,33 @@
 						<input type="text" id="iuser" name="username" required />
 						<span id="msg" class="hint">Only lowercase, at least 3 characters</span>
 						<br>
-						<br>
 					</p>
 					<p class="field">
 						<label for="ipass"> Password: </label>
 						<input type="password" id="ipass" name="password" required />
 						 <span class="hint">One uppercase, 1 symbol, 1 number, at least 8 characters</span>
 						<br>
-						<br>
 					</p>
 				    <p class="field">
 						<label for="ipass"> Repeat Password: </label>
 						<input type="password" id="ipass" name="passwordr" required />
-						<br>
 						<br>
 					</p>
 					<p class="field">
 						<label for="iname"> Name: </label>
 						<input type="text" id="iname" name="name" required />
 						<br>
-						<br>
 					</p>
 					<p class="field">
 						<label for="iphoto"> Choose Photo: </label>
 						<input type="file" id="iphoto" name="photo" required/>
 						<br>
-						<br>
 					</p>
-					
 					<div class="regBottons">
 						<ul>
 							<input type = "reset" value="Clear"/>
 							<input type = "submit" value="Register"/>
 						</ul>
-						<!--<a id="reg" href="registar.php">REGISTAR</a>-->
 					</div>
 			</form>
 		</div>
