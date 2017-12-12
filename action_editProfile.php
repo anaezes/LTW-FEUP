bb4a84abfbfb41dcce77536047b9d541e63f02f7
bernardoleite <up201404464@gcloud.fe.up.pt>
2017-12-12 21:40:20 +0000

    Options Form Edit User (Confirm)

--- action_editProfile.php
+++ action_editProfile.php
@@ -7,6 +7,7 @@
 	$name = $_POST['thename'];
 	$cpassword = $_POST['cpass'];
 	$npassword = $_POST['pass'];
+	$npasswordr = $_POST['passr'];
 	
 	$file_name = $_FILES['photo']['name'];
 	$file_type = $_FILES['photo']['type'];
@@ -16,20 +17,73 @@
 	
 	$errorEmpty = false;
 	$errorEmail = false;
-	
 
-	
-	if (empty($name) || empty($cpassword) || empty($npassword) || !isLoginCorrect($username, $cpassword) || !($file_size < 500000 && $file_type == 'image/png')) {
-		header('location:edit_profile.php');
-		$errorEmpty = true;
+	//alterar só pass : se nome==vazio && cpassword!=vazio && npassword!=vazio && npasswordr!=vazio && $file_name=vazio
+	if (empty($name) && !empty($cpassword) && !empty($npassword) && !empty($npasswordr) && isLoginCorrect($username, $cpassword) && empty($file_name) )
+	{
+		updatePassUser($username, $npassword);
+		header('location: index.php'); 
 		exit();
 	}
-	else 
+
+	//alterar só nome: se nome!=vazio && cpassword!=vazio && npassword==vazio && npasswordr==vazio && $file_name=vazio
+	else if (!empty($name) && !empty($cpassword) && empty($npassword) && empty($npasswordr) && isLoginCorrect($username, $cpassword) && empty($file_name) )
 	{
-		move_uploaded_file($file_tmp, $final_dest);
+		updateUser($username, $name);
+		header('location: index.php'); 
+		exit();
+	}
+
+	//alterar nome & pass: se nome!=vazio && cpassword!=vazio && npassword!=vazio && npasswordr!=vazio && $file_name=vazio
+	else if (!empty($name) && !empty($cpassword) && !empty($npassword) && !empty($npasswordr) && isLoginCorrect($username, $cpassword) && empty($file_name) )
+	{
 		updateUser($username, $name);
 		updatePassUser($username, $npassword);
 		header('location: index.php'); 
 		exit();
 	}
+
+	//alterar só imagem: sse nome=vazio && cpassword!=vazio && npassword=vazio && npasswordr=vazio && $file_name!=vazio
+	else if (empty($name) && !empty($cpassword) && empty($npassword) && empty($npasswordr) && isLoginCorrect($username, $cpassword) && ($file_size < 500000 && $file_type == 'image/png') )
+	{
+		move_uploaded_file($file_tmp, $final_dest);
+		header('location: index.php'); 
+		exit();
+	}
+
+	//alterar nome & imagem: se nome!=vazio && cpassword!=vazio && npassword=vazio && npasswordr=vazio && $file_name!=vazio
+	else if (!empty($name) && !empty($cpassword) && empty($npassword) && empty($npasswordr) && isLoginCorrect($username, $cpassword) && ($file_size < 500000 && $file_type == 'image/png') )
+	{
+		updateUser($username, $name);
+		move_uploaded_file($file_tmp, $final_dest);
+		header('location: index.php'); 
+		exit();
+	}
+
+	//alterar imagem & pass: se nome==vazio && cpassword!=vazio && npassword!=vazio && npasswordr!=vazio && $file_name!=vazio
+	else if (empty($name) && !empty($cpassword) && !empty($npassword) && !empty($npasswordr) && isLoginCorrect($username, $cpassword) && ($file_size < 500000 && $file_type == 'image/png') )
+	{
+		updatePassUser($username, $npassword);
+		move_uploaded_file($file_tmp, $final_dest);
+		header('location: index.php'); 
+		exit();
+	}
+
+	//alterar tudo: se nome!=vazio && cpassword!=vazio && npassword!=vazio && npasswordr!=vazio && $file_name!=vazio
+	else if (!empty($name) && !empty($cpassword) && !empty($npassword) && !empty($npasswordr) && isLoginCorrect($username, $cpassword) && ($file_size < 500000 && $file_type == 'image/png') )
+	{
+		updateUser($username, $name);
+		updatePassUser($username, $npassword);
+		move_uploaded_file($file_tmp, $final_dest);
+		header('location: index.php'); 
+		exit();
+	}
+
+	else {
+		header('location:edit_profile.php');
+		$errorEmpty = true;
+		exit();
+	}
+
+
 ?>
--- templates/user/editUser.php
+++ templates/user/editUser.php
@@ -4,8 +4,8 @@
 
     <form action="action_editProfile.php" method="POST" enctype="multipart/form-data">
       <p class="field">
-        <label for="name"> New name: </label>
-        <input type="text" id="name" name="thename" required />
+        <label for="name"> New name: (Optional)</label>
+        <input type="text" id="name" name="thename" />
         <span class="hint">Symbols are not allowed.</span>
         <br>
         <br>
@@ -19,23 +19,23 @@
       </p>
 
       <p class="field">
-         <label for="ipass"> New Password: </label>
-         <input type="password" id="ipass" name="pass"/ placeholder="Type your new Password" required>
+         <label for="ipass"> New Password: (Optional)</label>
+         <input type="password" id="ipass" name="pass"/ placeholder="Type your new Password">
          <span class="hint">One uppercase, 1 symbol, 1 number, at least 8 characters</span>
          <br>
          <br>
       </p>
 
        <p class="field">
-         <label for="ipass"> Repeat Password: </label>
-         <input type="password" id="inpass" name="passr"/ placeholder="Repeat your Password." required>
+         <label for="ipass"> Repeat Password: (Optional)</label>
+         <input type="password" id="inpass" name="passr"/ placeholder="Repeat your Password.">
          <span class="hint">Must match password</span>
          <br>
          <br>
       </p>
 
       <p class="field">
-        <label for="iphoto"> Change Photo: </label>
+        <label for="iphoto"> Change Photo: (Optional)</label>
         <input type="file" id="iphoto" name="photo"/>
         <br>
         <br>
